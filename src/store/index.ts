7ea0b7c604f9639be34143b0a36b0ddbf25dd6d6
Kohei Tsuchiya <doyahirohira@gmail.com>
2020-09-17 01:29:03 +0900

    モジュール分割した

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,47 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import axios from "axios";
 
-const BASE_STUDENT_URL = process.env.VUE_APP_STUDENT_URL;
-const GET_STUDENT_URL = BASE_STUDENT_URL + "/student";
-const GET_STUDENTS_URL = BASE_STUDENT_URL + "/students";
+import student from "./student";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    student: {},
-    students: []
-  },
-  getters: {
-    student: (state) => state.student,
-    students: (state) => state.students
-  },
-  mutations: {
-    setStudent(state, payload) {
-      state.student = payload.student;
-    },
-    setStudents(state, payload) {
-      state.students = payload.students;
-    }
-  },
-  actions: {
-    async getStudent({ commit }, id) {
-      try {
-        const res = await axios.get(GET_STUDENT_URL + "/" + id);
-        commit("setStudent", { student: res.data });
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    async getStudents({ commit }) {
-      try {
-        const res = await axios.get(GET_STUDENTS_URL);
-        commit("setStudents", { students: res.data });
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  },
-  modules: {}
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: { student }
 });
