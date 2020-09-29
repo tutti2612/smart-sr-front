feeb0d7f5896b2f9166d6121e9d1766889b5a15b
Kohei Tsuchiya <doyahirohira@gmail.com>
2020-09-30 02:12:28 +0900

    SaveStudentコンポーネントにバリデーションを追加

--- src/components/SaveStudent.vue
+++ src/components/SaveStudent.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="dialog" persistent max-width="600px">
+  <v-dialog v-model="state.dialog" persistent max-width="600px">
     <template v-slot:activator="{ on, attrs }">
       <v-btn :color="color" v-bind="attrs" v-on="on" class="mr-4">
         <slot></slot>
@@ -11,77 +11,94 @@
       </v-card-title>
       <v-card-text>
         <v-container>
-          <v-row>
-            <v-col cols="12">
-              <v-text-field
-                v-model="student.name"
-                :rules="state.nameRules"
-                label="氏名"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                v-model="student.classroom"
-                label="クラス"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                v-model="student.address"
-                label="住所"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                v-model="student.club"
-                label="部活動"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field v-model="student.tel" label="Tel"></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                v-model="student.email"
-                label="Email"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="6">
-              <v-text-field
-                v-model="student.age"
-                label="年齢"
-                suffix="歳"
-              ></v-text-field>
-              <v-select
-                :items="state.items"
-                item-text="sex"
-                item-value="value"
-                v-model="student.sex"
-                label="性別"
-              ></v-select>
-            </v-col>
-            <v-col cols="12" sm="6">
-              <v-text-field
-                v-model="student.height"
-                label="身長"
-                suffix="cm"
-              ></v-text-field>
-              <v-text-field
-                v-model="student.weight"
-                label="体重"
-                suffix="kg"
-              ></v-text-field>
-            </v-col>
-          </v-row>
+          <v-form ref="form" v-model="state.valid" lazy-validation>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="student.name"
+                  :rules="validationRules.name"
+                  label="氏名"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="student.classroom"
+                  label="クラス"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="student.address"
+                  label="住所"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="student.club"
+                  label="部活動"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="student.tel"
+                  :rules="validationRules.tel"
+                  label="Tel"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="student.email"
+                  :rules="validationRules.email"
+                  label="Email"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  v-model="student.age"
+                  :rules="validationRules.age"
+                  label="年齢"
+                  suffix="歳"
+                ></v-text-field>
+                <v-select
+                  :items="state.items"
+                  item-text="sex"
+                  item-value="value"
+                  v-model="student.sex"
+                  label="性別"
+                ></v-select>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  v-model="student.height"
+                  :rules="validationRules.height"
+                  label="身長"
+                  suffix="cm"
+                ></v-text-field>
+                <v-text-field
+                  v-model="student.weight"
+                  :rules="validationRules.weight"
+                  label="体重"
+                  suffix="kg"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-form>
         </v-container>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text @click="dialog = false">
+        <v-btn color="blue darken-1" text @click="state.dialog = false">
           Cancel
         </v-btn>
-        <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+        <v-btn
+          :disabled="!state.valid"
+          color="blue darken-1"
+          text
+          @click="save"
+        >
+          Save
+        </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -102,19 +119,28 @@ export default defineComponent({
           address: "",
           club: "",
           sex: "0",
-          height: null,
-          weight: null,
-          age: null,
+          height: "",
+          weight: "",
+          age: "",
           tel: "",
           email: ""
         };
       }
     }
   },
-  setup(props: any, { root }: any) {
-    const dialog = ref(false);
+  setup(props: any, context: any) {
+    const form = ref();
+    const validationRules = reactive({
+      name: [(v: string) => !!v || "Name is required"],
+      height: [(v: string) => /^\d*$/.test(v) || "height must be valid"],
+      weight: [(v: string) => /^\d*$/.test(v) || "weight must be valid"],
+      age: [(v: string) => /^\d*$/.test(v) || "age must be valid"],
+      tel: [(v: string) => /^\d*$/.test(v) || "tel must be valid"],
+      email: [(v: string) => /^$|(.+@.+\..+)/.test(v) || "E-mail must be valid"]
+    });
     const state = reactive({
-      nameRules: [(v: string) => !!v || "Name is required"],
+      dialog: false,
+      valid: false,
       items: [
         { sex: "男", value: "1" },
         { sex: "女", value: "2" },
@@ -123,21 +149,24 @@ export default defineComponent({
     });
 
     function save() {
+      if (!form.value.validate()) return;
+
       if (props.mode === "create") {
-        root.$store.dispatch("student/createStudent", {
+        context.root.$store.dispatch("student/createStudent", {
           student: props.student
         });
       }
       if (props.mode === "update") {
-        root.$store.dispatch("student/updateStudent", {
+        context.root.$store.dispatch("student/updateStudent", {
           student: props.student
         });
       }
-      dialog.value = false;
+      state.dialog = false;
     }
 
     return {
-      dialog,
+      form,
+      validationRules,
       state,
       save
     };
